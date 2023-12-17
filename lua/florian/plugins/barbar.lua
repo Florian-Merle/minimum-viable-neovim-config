684c406305018bb3e3dbc38c55bcdef285335c41
Florian Merle <florian.david.merle@gmail.com>
2023-12-17 21:55:14 +0100

    remove bufdelete and use barbar

--- lua/florian/plugins/barbar.lua
+++ lua/florian/plugins/barbar.lua
@@ -4,15 +4,16 @@ return {
     config = function ()
         require'barbar'.setup {
             animation = false,
-            auto_hide = true,
             icons = {
-                filetype = {
-                    enabled = false,
-                },
+                button = '',
             },
         }
 
         vim.keymap.set("n", "<tab>", ":BufferNext <CR>")
         vim.keymap.set("n", "<S-tab>", ":BufferPrevious <CR>")
-    end
+
+        vim.keymap.set("n", "<leader>x", ":BufferClose <CR>")
+        vim.keymap.set("n", "<leader>X", ":BufferCloseAllButCurrent <CR>")
+        vim.keymap.set("n", "<leader>W", ":BufferCloseAllButCurrent <CR> :BufferClose <CR>")
+    end,
 }
