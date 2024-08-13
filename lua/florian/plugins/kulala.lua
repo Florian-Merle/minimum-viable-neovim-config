74945accffdf9f436c611f47b4c81814813a5ad7
Florian Merle <florian.david.merle@gmail.com>
2024-08-13 14:23:20 +0200

    add kulala mapping

--- lua/florian/plugins/kulala.lua
+++ lua/florian/plugins/kulala.lua
@@ -6,5 +6,7 @@ return   {
     keys = {
         {"<leader>r", ":lua require('kulala').run()<CR>", ft = "http"},
         {"<leader>t", ":lua require('kulala').toggle_view()<CR>", ft = "http"},
+        {"[[", ":lua require('kulala').jump_prev()<CR>", ft = "http"},
+        {"]]", ":lua require('kulala').jump_next()<CR>", ft = "http"},
     },
 };
