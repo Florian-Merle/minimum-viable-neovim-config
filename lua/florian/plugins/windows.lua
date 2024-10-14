0ab36cac631fbea290333d357c1d8f3ae2cb5313
Florian Merle <florian.david.merle@gmail.com>
2024-10-14 09:00:32 +0200

    remove window equalize

--- lua/florian/plugins/windows.lua
+++ lua/florian/plugins/windows.lua
@@ -1,14 +1,7 @@
-vim.api.nvim_create_autocmd("VimResized", {
-    callback = function()
-        vim.cmd("WindowsEqualize")
-    end,
-})
-
 return {
     "anuvyklack/windows.nvim",
     dependencies = { "anuvyklack/middleclass" },
     opts = {},
-    event = "VimResized",
     cmd = { "WindowsMaximize", "WindowsEqualize" },
     keys = {
         { "<leader>z", "<CMD>WindowsMaximize<CR>" },
