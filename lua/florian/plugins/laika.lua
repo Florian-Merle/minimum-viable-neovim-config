fd7f009d2f54e61f684bbbb10794d0e462824083
Florian Merle <florian.david.merle@gmail.com>
2024-11-22 09:40:22 +0100

    add laika config

---
+++ lua/florian/plugins/laika.lua
@@ -0,0 +1,14 @@
+return {
+    dir = ".fmerle/laika",
+    name = "laika",
+    -- dependencies = {
+    --     'nvim-tree/nvim-web-devicons',
+    -- },
+    opts = {},
+    cmd = "Laika",
+    keys = {
+        -- {"<tab>", ":Laika buffer<CR>"},
+        -- {"<leader> ", ":Laika file<CR>"},
+        -- {"<leader>.", ":Laika text<CR>"},
+    },
+}
