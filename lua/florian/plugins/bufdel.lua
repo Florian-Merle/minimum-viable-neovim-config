d0ce809af1d29f8c2cc80baf029fbf47c424f12f
Florian Merle <florian.david.merle@gmail.com>
2026-01-26 11:43:43 +0100

    close buffers and window

--- lua/florian/plugins/bufdel.lua
+++ lua/florian/plugins/bufdel.lua
@@ -4,6 +4,12 @@ return {
         { "<leader>x", "<CMD>BufDel<CR>" },
         { "<leader>X", "<CMD>BufDelOthers<CR>" },
         { "<leader>W", "<CMD>BufDelAll<CR>" },
+        { "<leader>I", function ()
+            -- Keep one window
+            vim.cmd('only')
+            -- Close all buffers
+            vim.cmd("BufDelAll")
+        end},
         { "<leader>qq", function ()
             vim.cmd("BufDelAll")
             vim.cmd('confirm qall')
